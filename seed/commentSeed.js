d0c5aeb1c209a442d66e43be5c110a81e5cd6452
Jaxson20 <jaxsonfellhauerschool@gmail.com>
2023-09-17 15:44:48 -0500

    finished Handlebars

--- seed/commentSeed.js
+++ seed/commentSeed.js
@@ -1,23 +1,21 @@
-// seeds/comment-seeds.js
 const { CommentModel } = require('../models');
 
 const commentData = [
   {
     content: 'Great post!',
-    user_id: 1, // User ID for the commenter (adjust as needed).
-    blog_post_id: 1, // Blog post ID for the associated post (adjust as needed).
+    user_id: 1, 
+    blog_post_id: 1, 
   },
   {
     content: 'I have a question about this topic.',
-    user_id: 2, // User ID for the commenter (adjust as needed).
-    blog_post_id: 1, // Blog post ID for the associated post (adjust as needed).
+    user_id: 2, 
+    blog_post_id: 1, 
   },
   {
     content: 'Interesting insights!',
-    user_id: 3, // User ID for the commenter (adjust as needed).
-    blog_post_id: 2, // Blog post ID for the associated post (adjust as needed).
+    user_id: 3, 
+    blog_post_id: 2, 
   },
-  // Add more comments as needed.
 ];
 
 const seedComments = () => CommentModel.bulkCreate(commentData);
